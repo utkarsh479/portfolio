460ab0c43fbb8282215d559e845654de0d2e993d
utkarsh479 <khatriutkarsh545@gmail.com>
2025-07-25 10:03:03 +0530

    Create projectdata.js

---
+++ src/components/projectdata.js
@@ -0,0 +1,14 @@
+// src/data/projectdata.js
+export const ProjectList = [
+  {
+    id: 1,
+    Title: "My First Project",
+    Description: "Description here",
+    Img: "/your-img.png",
+    TechStack: ["React", "Tailwind"],
+    Features: ["Feature A", "Feature B"],
+    Github: "https://github.com/utkarsh/project1",
+    Link: "https://live-demo-link.com"
+  },
+  ...
+];
