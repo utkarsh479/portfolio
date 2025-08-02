f284b8698a86b80f8d2e537dc8d828ae11a8c915
utkarsh479 <khatriutkarsh545@gmail.com>
2025-08-02 09:16:31 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -77,7 +77,7 @@ const SocialLink = memo(({ icon: Icon, link }) => (
 const TYPING_SPEED = 100;
 const ERASING_SPEED = 50;
 const PAUSE_DURATION = 2000;
-const WORDS = ["Software Developer", "Tech Enthusiast"];
+const WORDS = ["A Computer Science Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
   { icon: Github, link: "https://github.com/utkarsh479" },
